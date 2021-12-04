9190902f7d9d81fce0295d2f41e52dc69b52ca12
David J Ortiz <87109541+MariachiMES@users.noreply.github.com>
2021-12-03 23:23:38 -0600

    server set up with model

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
-
-const db = require("./models");
-
 const app = express();
-
-app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbworkout", {
   useNewUrlParser: true,
 });
-
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`Workout tracker running on port ${PORT}`);
 });
